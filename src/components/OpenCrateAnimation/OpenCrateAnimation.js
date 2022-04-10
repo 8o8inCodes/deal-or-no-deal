cf83a40c6713afe0b5e669886247ba7ba1d268fb
Leonid Haimov <8o8inSquares@gmail.com>
2022-04-10 15:24:38 +0300

    Add crate opening animation

--- src/components/OpenCrateAnimation/OpenCrateAnimation.js
+++ src/components/OpenCrateAnimation/OpenCrateAnimation.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import s from "./OpenCrateAnimation.module.css";
 const OpenCrateAnimation = ({ crate, onAnimationFinish, onClose }) => {
+	const animationRef = useRef(null);
+	const [closeDisabled, setCloseDisabled] = useState(true);
 	useEffect(() => {
-		onAnimationFinish();
+		animationRef.current.addEventListener("animationend", animationEnded);
+		// onAnimationFinish();
 	}, []);
+
+	const animationEnded = () => {
+		setCloseDisabled(false);
+		onAnimationFinish();
+	};
 	return (
 		<div className={s.backdrop}>
 			<div className={s.modal}>
-				<h1>${crate.money}</h1>
-				<button className={s.button} onClick={onClose}>
+				<div className={s.crateImage}>
+					<h1 ref={animationRef}>${crate.money}</h1>
+				</div>
+				<button className={s.button} onClick={onClose} disabled={closeDisabled}>
 					Close
 				</button>
 			</div>
